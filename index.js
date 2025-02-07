ffbd02db750259ccc95a4287e8c6c2dc380c74e4
HugoW <hugo.wilinski@elev.ga.lbs.se>
2025-02-07 09:52:30 +0100

    Fixes by Erik

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
-const userRouter = require("/routes/users");
+const userRouter = require("/users");
 
 const app = express();
 
