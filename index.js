72408312ff8b14d1b1babb46d5bd7bece3388f9a
HugoW <hugo.wilinski@elev.ga.lbs.se>
2025-02-07 09:27:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const userRouter = require("/routes/users");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    },
+  })
+);
+app.set("view engine", "ejs");
+
+const requireAuth = (req, res, next) => {
+  if (!req.session.userId) {
+    return res.redirect("/login");
+  }
+  next();
+};
+
+app.use("/users", userRouter);
+
+app.get("/", requireAuth, (req, res) => {
+  res.render("home", { user: req.session.user });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  const { username, password, rememberMe } = req.body;
+  const user = users.find(
+    (u) => u.username === username && u.password === password
+  );
+  if (!user) return res.status(400).send("Invalid credentials");
+  req.session.userId = user.id;
+  req.session.user = user;
+  if (rememberMe) {
+    req.session.cookie.maxAge = 30 * 24 * 60 * 60 * 1000;
+  }
+  res.redirect("/");
+});
+
+app.post("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) return res.redirect("/");
+    res.clearCookie("connect.sid");
+    res.redirect("/login");
+  });
+});
+
+const users = [];
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
